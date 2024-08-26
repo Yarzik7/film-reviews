87649bafea4d20027c33f98b925be4e54a8abdc5
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-08-26 23:52:23 +0300

    Update SectionWithContainer.jsx

--- src/components/SectionWithContainer/SectionWithContainer.jsx
+++ src/components/SectionWithContainer/SectionWithContainer.jsx
@@ -1,6 +1,5 @@
 import Section from "./Section/Section";
 import Container from "./Container/Container";
-// import css from "./SectionWithContainer.module.css";
 
 const SectionWithContainer = ({ children }) => {
   return (
