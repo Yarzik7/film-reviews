69ff37e5fc58eb44fa8b2c553b08554b8f135931
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-09-07 22:01:00 +0300

    Update Container.jsx

--- src/components/SectionWithContainer/Container/Container.jsx
+++ src/components/SectionWithContainer/Container/Container.jsx
@@ -1,7 +1,11 @@
 import css from "./Container.module.css";
 
 const Container = ({ className = "", children }) => {
-  return <div className={css.container + " " + className}>{children}</div>;
+  return (
+    <div className={[css.container, className].join(" ").trim()}>
+      {children}
+    </div>
+  );
 };
 
 export default Container;
